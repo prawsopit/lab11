438a23282040316a1ba44b3e89ec6d144352142c
prawsopit <prawsopit@gmail.com>
2023-01-03 15:46:30 +0700

    11.3

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,37 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main(){
+    ifstream score;
+    score.open("score.txt") ;
+    int x ;
+    x=0 ;
+    float y ; 
+    y=0 ;
+    float a ;
+    float b ;
+    b=0 ;
+    float c ;
+      
+    string textline ;
+     while(getline(score,textline))
+     {
+        y += atof(textline.c_str()) ;
+      b += pow(atof(textline.c_str()),2);
+      
+        x++ ;
+     }
+      a= y/x ;
+      c=pow(b/x-a*a,0.5);
+
+
+    cout << "Number of data = " << x << "\n" ; 
+    cout << setprecision(3);
+    cout << "Mean = "<< a << "\n";
+    cout << "Standard deviation = " << c;
+score.close() ;
+}
